67f2e1486af86803b7017bd77bec7f8f8223ae89
Lukas <lukas.blanchot@hotmail.fr>
2022-09-08 23:02:14 +0200

    ajout listener

--- src/fr/silenix/tntwars/main.java
+++ src/fr/silenix/tntwars/main.java
@@ -1,7 +1,22 @@
 package fr.silenix.tntwars;
 
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class main extends JavaPlugin{
+import fr.silenix.tntwars.listener.DamageListeners;
+import fr.silenix.tntwars.listener.PlayerListeners;
 
+
+
+
+public class main extends JavaPlugin{
+	
+	
+	
+	public void onEnable() {
+	PluginManager pm = getServer().getPluginManager();
+	pm.registerEvents(new PlayerListeners(this) , this);
+	pm.registerEvents(new DamageListeners(this), this);
+	
+	}
 }
