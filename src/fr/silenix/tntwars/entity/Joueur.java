d556b1dec30e40b5bd08fb0b675712f9bfb23903
Lukas <lukas.blanchot@hotmail.fr>
2022-09-08 22:28:49 +0200

    getter setter joueur/tnt

--- src/fr/silenix/tntwars/entity/Joueur.java
+++ src/fr/silenix/tntwars/entity/Joueur.java
@@ -1,10 +1,35 @@
 package fr.silenix.tntwars.entity;
 
+import org.bukkit.entity.Player;
 
 public class Joueur {
 
 	private Equipe Equipe;
-	//private Player Player;
+	private Player Player;
 	private Kit Kit;
+	
+	
+	
+	
+	public Equipe getEquipe() {
+		return Equipe;
+	}
+	public void setEquipe(Equipe equipe) {
+		Equipe = equipe;
+	}
+	public Player getPlayer() {
+		return Player;
+	}
+	public void setPlayer(Player player) {
+		Player = player;
+	}
+	public Kit getKit() {
+		return Kit;
+	}
+	public void setKit(Kit kit) {
+		Kit = kit;
+	}
+	
+	
 	
 }
--- src/fr/silenix/tntwars/entity/Tnt.java
+++ src/fr/silenix/tntwars/entity/Tnt.java
@@ -5,9 +5,38 @@ import org.bukkit.Location;
 import Enum.EtatTNT;
 
 public class Tnt {
-private Equipe Equipe;
-private Location Location;
-private int Timer;
-private EtatTNT Etat;
+	private Equipe Equipe;
+	private Location Location;
+	private int Timer;
+	private EtatTNT Etat;
+	
+	
+	
+	public Equipe getEquipe() {
+		return Equipe;
+	}
+	public void setEquipe(Equipe equipe) {
+		Equipe = equipe;
+	}
+	public Location getLocation() {
+		return Location;
+	}
+	public void setLocation(Location location) {
+		Location = location;
+	}
+	public int getTimer() {
+		return Timer;
+	}
+	public void setTimer(int timer) {
+		Timer = timer;
+	}
+	public EtatTNT getEtat() {
+		return Etat;
+	}
+	public void setEtat(EtatTNT etat) {
+		Etat = etat;
+	}
+	
+	
 
 }
